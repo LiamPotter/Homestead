d9815c3612325cec7b8f096b6f010d78cdc78b3a
cosmo5 <chris_os_011@hotmail.com>
2017-02-07 14:32:52 +1000

    started editor plugin

---
+++ Assets/Editor/Farming/Farming Plots.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class _FarmingPlots : EditorWindow {
+
+    int rows = 0;
+    int columns = 0;
+    [MenuItem("Farming/CreateFarm")] 
+    static void Init()
+    {
+        GetWindow(typeof(_FarmingPlots));
+    }
+
+    void OnGUI()
+    {
+        GUIContent content = new GUIContent("Rows");
+        GUIContent content2 = new GUIContent("Columns");
+
+        float minWidth;
+        float maxWidth;
+
+        GUIStyle style = new GUIStyle();
+        style.CalcMinMaxWidth(content, out minWidth, out maxWidth);
+        style.fixedWidth = minWidth;
+        style.alignment = TextAnchor.MiddleCenter;
+        style.fontSize = 12;
+   
+        
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label(content);
+        
+        int.TryParse((GUI.TextField(new Rect(Screen.width / 2 + 20, Screen.height - 30, minWidth, 20), "", Mathf.RoundToInt( minWidth))), out rows);
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label(content2);
+        int.TryParse((GUILayout.TextField(columns.ToString(), Mathf.RoundToInt(minWidth))), out columns);
+        GUILayout.EndHorizontal();
+
+
+
+    }
+
+}
